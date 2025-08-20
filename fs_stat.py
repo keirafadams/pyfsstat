635bbee1f3c8711507d84990581e068f580d949a
kfadams <keira.f.adams@gmail.com>
2025-08-20 13:38:34 -0700

    More docstrings, stubs. Anonymizer working.

---
+++ fs_stat.py
@@ -0,0 +1,8 @@
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog='PyFSStat',
+        description='File System Metadata Crawler',
+        epilog='Created by Keira Adams, 2025')
